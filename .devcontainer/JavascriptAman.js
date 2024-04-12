35645e0b6ad46b340c758b7bce1d16e5643b779e
Aman Mehta <96304523+AmanMehta199816@users.noreply.github.com>
2024-04-12 14:08:36 +0000

    javascript

---
+++ .devcontainer/JavascriptAman.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="mehtaaman8108@gmail.com"
+var accountPassword="12345"
+accountCity="Delhi"
+
+//accountId=2// Not allowed
+
+accountEmail="mehtaaman8108@gmail.com"
+accountPassword="12345"
+accountCity="Delhi"
+console.log(accountId);
+console.table(accountId,accountEmail,accountPassword,accountCity)
